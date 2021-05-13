bc61b8e8f610121786d6d2d08f318ed3eefd9e34
da <elosogamer28@gmail.com>
2021-05-13 18:30:08 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import React,{useState} from 'react';
 import './App.css';
 import Header from './componentes/header';
 import FormularioTareas from './componentes/FormularioTareas';
 
 const App = () => {
+
+  const [tareas, cambiarTareas]=useState(
+    [
+      {
+        id:1,
+        text: 'Lavar',
+        completada:false
+      },{
+        id:2,
+        text: 'comprar armario',
+        completada:false
+      }
+    ]);
+
+
+    console.log(tareas);
+
   return (
     <div className="contenedor">
       <Header />
-      <FormularioTareas />
+      <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
     </div>
   );
 }
+
 
 export default App;
--- src/componentes/FormularioTareas.js
+++ src/componentes/FormularioTareas.js
@@ -1,27 +1,45 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 
-const FormularioTareas = () => {
+const FormularioTareas = ({ tareas, cambiarTareas }) => {
+    const [inputTarea, cambiarImputTareas] = useState('');
+
+    const handelInput = (e) => {
+        cambiarImputTareas(e.target.value);
+    };
+
+    const handeleSubmit = (e) => {
+        // * no se refresca la pagina...
+        e.preventDefault();
+
+        cambiarTareas([
+            ...tareas,
+            {
+                id: 3,
+                texto: inputTarea,
+                completada: false,
+            },
+        ]);
+    };
+
     return (
-        <form action="#" className='formulario-tareas'>
+        <form action="" className="formulario-tareas" onSubmit={handeleSubmit}>
             <input
+                value={inputTarea}
                 type="text"
-                className='formulario-tareas__input'
+                className="formulario-tareas__input"
                 placeholder="Escribe Una tarea"
+                onChange={(e) => handelInput(e)}
             />
-            <button
-                type="submit"
-                className='formulario-tareas__btn'
-            >
+            <button type="submit" className="formulario-tareas__btn">
                 <FontAwesomeIcon
                     icon={faPlusSquare}
-                    className='formulario-tareas__icono' />
+                    className="formulario-tareas__icono-btn"
+                />
             </button>
         </form>
     );
-}
-
-
+};
 
 export default FormularioTareas;
